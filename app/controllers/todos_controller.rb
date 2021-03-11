f21cafb481541c908d9668b7a32cc76fad55c747
Sarvesh <sarvesh4232@gmail.com>
2021-03-11 15:15:13 +0530

    View Particular Todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,5 +1,11 @@
 class TodosController < ApplicationController
   def index
-    render plain: Todo.all.to_a
+    render plain: Todo.all.map { |todo| todo.to_plesant_string }
+  end
+
+  def show
+    id = params[:id]
+    todo = Todo.find(id)
+    render plain: todo.to_plesant_string
   end
 end
--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,2 +1,6 @@
 class Todo < ActiveRecord::Base
+  def to_plesant_string
+    is_completed = completed ? "[x]" : "[ ]"
+    "#{id}. #{todo_text} #{is_completed}"
+  end
 end
